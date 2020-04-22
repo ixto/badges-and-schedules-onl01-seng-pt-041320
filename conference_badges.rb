3e474268705553070f990ef3ad9ed15203855582
Joey <joey.alzamora@gmail.com>
2020-04-22 22:43:02 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,33 +1,33 @@
 
-def badge_maker(name)
-  puts "Hello, my name is #{name}."
-  "Hello, my name is Arel."
-end
+# def badge_maker(name)
+#   puts "Hello, my name is #{name}."
+#   "Hello, my name is Arel."
+# end
 
-def batch_badge_creator(array)
-    new_array = [] 
-    array.each do |name|
-      new_array.push("Hello, My name is #{name}.")
- end
-  return new_array
-end
+# def batch_badge_creator(array)
+#     new_array = [] 
+#     array.each do |name|
+#       new_array.push("Hello, My name is #{name}.")
+# end
+#   return new_array
+# end
 
-def assign_rooms(attendees)
-  new_array =[]
-  room = 1 
-  attendees.each do |name|
-    new_array.push("Hello, #{name}! You'll be assigned to room #{room}!")
-    room += 1 
-  end
-  return new_arry
-end
+# def assign_rooms(attendees)
+#   new_array =[]
+#   room = 1 
+#   attendees.each do |name|
+#     new_array.push("Hello, #{name}! You'll be assigned to room #{room}!")
+#     room += 1 
+#   end
+#   return new_arry
+# end
 
-def printer(attendees)
-  new_array =[]
-  room = 1 
-  attenddes.each do |name|
-    puts ("Hello, my name is #{name}.")
-    puts ("Hello, #{name}! You'll be assigned to room #{room}!")
-    room += 1 
-  end
-end
+# def printer(attendees)
+#   new_array =[]
+#   room = 1 
+#   attenddes.each do |name|
+#     puts ("Hello, my name is #{name}.")
+#     puts ("Hello, #{name}! You'll be assigned to room #{room}!")
+#     room += 1 
+#   end
+# end
