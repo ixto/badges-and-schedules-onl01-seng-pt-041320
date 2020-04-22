ee08e483c84f7ae8dc08090808cab056fb823bfe
Joey <joey.alzamora@gmail.com>
2020-04-22 22:49:25 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -50,7 +50,7 @@ def batch_badge_creator(array)
    new_array =[]
    count = 1 
    array.each do |name|
-     new_array.push(Hello, #{name}! You'll be assigned to room #{room}!")
+     new_array.push("Hello, #{name}! You'll be assigned to room #{room}!")
      count += 1 
    end
     return new_array
